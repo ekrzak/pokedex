94b2b334cc7645c23c82d6ec50e974a7d919d561
estebankrzak <esteban.rk@hotmail.com>
2022-04-06 18:48:27 -0300

    Add rendering for all pokemons

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,13 @@
-function searchPokemonsInApi() {
+const $pageActions = document.querySelector('#page-actions');
+let nextTenPokemonsUrl = '';
+let previousTenPokemonsUrl;
+
+function searchPokemonsInApi(url) {
+  return fetch(url)
+    .then(response => response.json());
+}
+
+function searchFirstTenPokemons() {
   return fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=10')
     .then(response => response.json());
 }
@@ -8,6 +17,16 @@ function searchPokemonInfoInApi() {
 }
 
 function renderPokemonsNames(pokemons) {
+  nextTenPokemonsUrl = pokemons.next;
+  previousTenPokemonsUrl = pokemons.previous;
+
+  if (previousTenPokemonsUrl === null) {
+    document.querySelector('#page-previous').parentElement.classList.add('disabled');
+    document.querySelector('#page-previous').classList.remove('text-danger');
+  } else {
+    document.querySelector('#page-previous').parentElement.classList.remove('disabled');
+    document.querySelector('#page-previous').classList.add('text-danger');
+  }
 
   document.querySelectorAll('.rounded-pill').forEach(function(pokemonButton, index) {
     const pokemonId = pokemons.results[index].url.replace(/https:\/\/pokeapi\.co\/api\/v2\/pokemon\//, '').replace(/\//, '');
@@ -21,9 +40,16 @@ function renderPokemonInfo() {
 
 }
 
-function handlePagination() {
+function handlePagination(event) {
+  if (event.target.id === 'page-next') {
+    searchPokemonsInApi(nextTenPokemonsUrl).then(renderPokemonsNames);
+  }
+
+  if (event.target.id === 'page-previous') {
+    searchPokemonsInApi(previousTenPokemonsUrl).then(renderPokemonsNames);
+  }
 
 }
 
-searchPokemonsInApi().then(renderPokemonsNames);
-
+searchFirstTenPokemons().then(renderPokemonsNames);
+$pageActions.onclick = handlePagination;
